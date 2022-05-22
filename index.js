c604f798a0fd1493eea526eaeb7d042a8540ff5a
PrantaDas <prantodas043@gmail.com>
2022-05-23 01:49:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dub8z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('db is connected');
+    }
+
+    finally{
+
+    }
+};
+
+
+
+
+
+// root api
+app.get('/', (req, res) => {
+    res.send('Hello from Mason hut server');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port ', port);
+});
+
+run().catch(console.dir);
