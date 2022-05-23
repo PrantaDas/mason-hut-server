1051133ebdc12ed409aba0abf56e5ca10796baf9
PrantaDas <prantodas043@gmail.com>
2022-05-23 17:48:41 +0600

    order collection and quantity manage done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -18,13 +18,78 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         console.log('db is connected');
+
+        const toolsCollection = client.db('mason_hut').collection('tools');
+
+        const usersCollection = client.db('mason_hut').collection('users');
+
+        const ordersCollection = client.db('mason_hut').collection('orders');
+
+
+        // verify jwt token
+
+
+        // load all tools 
+
+        app.get('/tools', async (req, res) => {
+            const tools = await toolsCollection.find({}).toArray();
+            res.send(tools);
+        });
+
+        // load single tools by id
+
+        app.get('/tools/:id', async (req, res) => {
+            const authHeader = req.headers.authorization;
+            const id = req.params.id
+            // console.log(authHeader);
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
+        // update quantity after order
+
+        app.put('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const tool = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: tool
+            }
+            const result = await toolsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        });
+
+        // order collection
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        });
+
+        // issuing jwt token for login and signup
+
+        app.post('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const userInfo = req.body;
+            const filter = { email: email };
+            const optiions = { upsert: true };
+            const updatedDoc = {
+                $set: userInfo
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc, optiions);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, token });
+        });
     }
 
-    finally{
+    finally {
 
     }
 };
